7b18739ab6c1338420ebcf612436439b84e3edde
zero88 <sontt246@gmail.com>
2021-02-09 13:44:50 +0700

    - Update benchmark test

--- benchmark/discovery-network.js
+++ benchmark/discovery-network.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export default function() {
-  let res = http.get('http://zpi:8888/api/discovery/bacnet/network');
+  let res = http.get('http://zpi:8888/api/discover/bacnet/network');
   check(res, { 'status was 200': (r) => r.status === 200 });
   sleep(1);
 }
